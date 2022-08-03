0400834b0cc79d005452c4b917d4bc963daba23e
sahelorang <sahel12345zelzeleh@gmail.com>
2022-08-03 21:42:19 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SignUp from "./components/signUp";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/signUp.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from "react";
+import { validate } from "./validate";
+import styles from "./signUp.module.css";
+
+const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+
+  useEffect(() => {
+    setErrors(validate(data));
+    // console.log(errors);
+  }, [data, touched]);
+
+  const changeHandler = (event) => {
+    if (event.target.name === "isAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+  };
+
+  const focusHandler = (event) => {
+    setTouched({ ...touched, [event.target.name]: true });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!Object.keys(errors).length) {
+      alert("you sign up in successfully!");
+    } else {
+      alert("invalid data!");
+
+      setTouched({
+        name: true,
+        email: true,
+        password: true,
+        confirmPassword: true,
+        isAccepted: true,
+      });
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={submitHandler} className={styles.formContainer}>
+        <h2 className={styles.header}>SignUp</h2>
+        <div className={styles.formField}>
+          <label>name</label>
+          <input
+            className={
+              errors.name && touched.name
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            name="name"
+            type="text"
+            value={data.name}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.name && touched.name && <span>{errors.name}</span>}
+        </div>
+        <div className={styles.formField}>
+          <label>Email</label>
+          <input
+            className={
+              errors.email && touched.email
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            name="email"
+            type="text"
+            value={data.email}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.email && touched.email && <span>{errors.email}</span>}
+        </div>
+        <div className={styles.formField}>
+          <label>Password</label>
+          <input
+            className={
+              errors.password && touched.password
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            name="password"
+            type="password"
+            value={data.password}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.password && touched.password && (
+            <span>{errors.password}</span>
+          )}
+        </div>
+        <div className={styles.formField}>
+          <label>confirmPassword</label>
+          <input
+            className={
+              errors.confirmPassword && touched.confirmPassword
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            name="confirmPassword"
+            type="password"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.confirmPassword && touched.confirmPassword && (
+            <span>{errors.confirmPassword}</span>
+          )}
+        </div>
+        <div className={styles.formField}>
+          <div className={styles.checkBoxContainer}>
+            <label>I Accet terms of privacy policy</label>
+            <input
+              name="isAccepted"
+              type="checkbox"
+              value={data.isAccepted}
+              onChange={changeHandler}
+              onFocus={focusHandler}
+            />
+          </div>
+          {errors.isAccepted && touched.isAccepted && (
+            <span>{errors.isAccepted}</span>
+          )}
+        </div>
+        <div className={styles.formButtons}>
+          <a href="#">Login</a>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
